2870b22abbc13420dd1c58f1f78b687263ee5981
Laurent Lasudry <lasudry@gmail.com>
2021-08-17 10:55:57 +0200

    Blacked

--- setup.py
+++ setup.py
@@ -5,17 +5,21 @@ from setuptools import setup, find_packages
 def read(*pathnames):
     return open(os.path.join(os.path.dirname(__file__), *pathnames)).read()
 
-version = '1.1.dev0'
 
-setup(name='collective.complexrecordsproxy',
-      version=version,
-      description="Providing a proxy class for plone.registry allowing IObject fields to be stored as separate records using the plone.registry collection support.",
-      long_description='\n'.join([
-          read('README.rst'),
-          read('CHANGES.rst'),
-          ]),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
+version = "1.1.dev0"
+
+setup(
+    name="collective.complexrecordsproxy",
+    version=version,
+    description="Providing a proxy class for plone.registry allowing IObject fields to be stored as separate records using the plone.registry collection support.",
+    long_description="\n".join(
+        [
+            read("README.rst"),
+            read("CHANGES.rst"),
+        ]
+    ),
+    # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
         "Framework :: Plone",
         "Framework :: Plone :: 4.3",
         "Framework :: Plone :: 5.0",
@@ -27,21 +31,17 @@ setup(name='collective.complexrecordsproxy',
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        ],
-      keywords='configuration registry',
-      author='Sune Broendum Woeller',
-      author_email='sune@woeller.dk',
-      url='http://pypi.python.org/pypi/collective.complexrecordsproxy',
-      license='GPLv2+',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages = ['collective'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'plone.registry>=1.0',
-          ''
-      ],
-      entry_points="""
+    ],
+    keywords="configuration registry",
+    author="Sune Broendum Woeller",
+    author_email="sune@woeller.dk",
+    url="http://pypi.python.org/pypi/collective.complexrecordsproxy",
+    license="GPLv2+",
+    packages=find_packages(exclude=["ez_setup"]),
+    namespace_packages=["collective"],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=["setuptools", "plone.registry>=1.0", ""],
+    entry_points="""
       """,
-      )
+)
